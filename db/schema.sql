d65c0e26d9336c6686a902f94d7470d7fa0ccac7
Harry Spencer <haspencer4@gmail.com>
2018-01-07 22:11:55 -0600

    most stuff created but not done

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers_db
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	date_id TIMESTAMP,
+	PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name) VALUES ('Onion-tended Consequences Burger');
+INSERT INTO burgers(burger_name) VALUES ('Olive and Let Die Burger');
+INSERT INTO burgers(burger_name) VALUES ('Fig-eta Bout It Burger');
+INSERT INTO burgers(burger_name) VALUES ('If Looks Could Kale Burger');
+INSERT INTO burgers(burger_name) VALUES ('Open Sesame Burger');
